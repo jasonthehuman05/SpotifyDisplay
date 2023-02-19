60ea6e0670ec9938bce28ec4ef020f2c972f8586
Jason Chesters-salt <43147625+jasonTheHuman2101@users.noreply.github.com>
2023-02-19 16:58:37 +0000

    reverting

--- SpotifyHandler/SpotifyHandler.cs
+++ SpotifyHandler/SpotifyHandler.cs
@@ -1,12 +1,17 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+using System.IO;
 using System.Threading.Tasks;
+using SpotifyAPI.Web;
 
 namespace SpotifyDisplay.SpotifyHandler
 {
     internal class SpotifyHandler
     {
+        public SpotifyHandler()
+        {
+            //Create spotifyapi object
+            SpotifyClient spotify = new SpotifyClient(File.ReadAllText("clientid.txt"));
+        }
     }
 }
